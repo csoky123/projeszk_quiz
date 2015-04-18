de5fd39f4679b1389eda313d874f11699c15be09
zaasaai <zaasaai@nylw46.inf.elte.hu>
2015-04-18 15:34:36 +0200

    first

---
+++ src/quiz_client/Quiz_client.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz_client;
+
+/**
+ *
+ * @author zaasaai
+ */
+public class Quiz_client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
