bef36675673af5bcce849b311487af783ac71c98
zaasaai <zaasaai@nylw46.inf.elte.hu>
2015-04-18 15:37:06 +0200

    Logic created

---
+++ src/quiz_client/Logic.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz_client;
+
+/**
+ *
+ * @author zaasaai
+ */
+public class Logic {
+    
+}
