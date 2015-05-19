7f77dc5e584a13d86f3cdd3e76baa20658124d58
csoky123 <csoky123@zak-laptop>
2015-05-19 09:56:17 +0200

    javadoc comments added to ConnectionHandler package

--- src/ConnectionHandler/QuizQuestion.java
+++ src/ConnectionHandler/QuizQuestion.java
@@ -6,14 +6,29 @@ public class QuizQuestion{
     private final String question;
     private final ArrayList<String> answers;
 
+    
+    /**
+     * A függvény visszaadja a jelenlegi kérdést.
+     * @return A kérédés String
+     */
     public String getQuestion() {
         return question;
     }
 
+    /**
+     * A függvény visszaadja a jelenlegi lehetséges válaszokat.
+     * @return A válaszok egy ArrayList<String> objektumban
+     */
     public ArrayList<String> getAnswers() {
         return answers;
     }
     
+    /**
+     * Konstruktor, mely egy kérdésből es 
+     * a válaszokat tartalmazó ArrayListből példányosít egy QuizQuestion objektumot
+     * @param q A kérdés
+     * @param answers A válaszok
+     */
     public QuizQuestion(String q, ArrayList<String> answers) {
         question = q;
         this.answers = answers;
--- src/ConnectionHandler/ServerConnection.java
+++ src/ConnectionHandler/ServerConnection.java
@@ -16,13 +16,21 @@ public class ServerConnection extends Thread {
     Scanner sc;
     PrintWriter pw;
 
-    
+    /**
+     * Az osztály kezeli a szerverrel a kapcsolatot.
+     * TCP kapcsolatot tart fent.
+     * @throws IOException Sikertelen írás/olvasás esetén dobja.
+     */
     public ServerConnection() throws IOException {
         s = new Socket(IP, PORT);
         sc = new Scanner(s.getInputStream());
         pw = new PrintWriter(s.getOutputStream());
     }
     
+    /**
+     * Új kérdést generáltat a szerverrel.
+     * @return Az új kérdés
+     */
     public String nextQuestion() {
         System.out.println("ServerConnection: nextQuestion()");
         pw.println("new");
@@ -40,11 +48,20 @@ public class ServerConnection extends Thread {
         return currentQuestion.getQuestion();
     };
     
+    /**
+     * A jelenlegi kérdésre adható válaszokat adja vissza
+     * @return A válaszok egy ArrayList objektumban
+     */
     public ArrayList<String> getAnswers() {
         System.out.println("ServerConnection: getAnswers()");
         return currentQuestion.getAnswers();
     };
     
+    /**
+     * A paraméterként kapott választ továbbítja a szervernek és megmondja, hogy helyes-e.
+     * @param answer A válasz, melynek helyességét ellenőrzi.
+     * @return Igaz, ha helyes, hamis, ha nem.
+     */
     public Boolean isCorrect(String answer) {
 	System.out.println("ServerConnection: isCorrect()");
         pw.println("isCorrect");
@@ -53,6 +70,10 @@ public class ServerConnection extends Thread {
         return sc.nextInt() == 1;
     }
     
+    /**
+     * Visszaadja a jelenlegi kérdésre a helyes választ.
+     * @return A helyes válasz.
+     */
     public String getCorrectAnswer() {
         System.out.println("ServerConnection: getCorrectAnswer()");
         pw.println("getCorrectAnswer");
